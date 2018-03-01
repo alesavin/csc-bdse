5f32fcc1f9dffaba0304d72795f1c7a3cf9c19e2
equivalence1 <equivalence1@gmail.com>
2018-03-01 17:17:25 +0300

    concurrentPuts test + README updated

--- bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest2.java
+++ bdse-integration-tests/src/test/java/ru/csc/bdse/kv/KeyValueApiHttpClientTest2.java
@@ -1,15 +1,19 @@
 package ru.csc.bdse.kv;
 
-import org.junit.ClassRule;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
+import ru.csc.bdse.util.Constants;
 import ru.csc.bdse.util.Env;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.UUID;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test have to be implemented
@@ -18,26 +22,69 @@ import static java.time.temporal.ChronoUnit.SECONDS;
  */
 public class KeyValueApiHttpClientTest2 {
 
-    @ClassRule
-    public static final GenericContainer node = new GenericContainer(
-            new ImageFromDockerfile()
-                    .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
-                            ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
-                    .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
-            .withEnv(Env.KVNODE_NAME, "node-0")
-            .withExposedPorts(8080)
-            .withStartupTimeout(Duration.of(30, SECONDS));
+    private static String nodeName;
+    private static GenericContainer node;
+    private static KeyValueApi api;
 
-    private KeyValueApi api = newKeyValueApi();
+    @BeforeClass
+    public static void setup() {
+        nodeName = "test-node" + UUID.randomUUID().toString().substring(4);
+        node = (GenericContainer) new GenericContainer(
+                new ImageFromDockerfile()
+                        .withFileFromFile("target/bdse-kvnode-0.0.1-SNAPSHOT.jar", new File
+                                ("../bdse-kvnode/target/bdse-kvnode-0.0.1-SNAPSHOT.jar"))
+                        .withFileFromClasspath("Dockerfile", "kvnode/Dockerfile"))
+                .withEnv(Env.KVNODE_NAME, nodeName)
+                .withExposedPorts(8080)
+                .withStartupTimeout(Duration.of(30, SECONDS))
+                .withFileSystemBind("/var/run/docker.sock", "/var/run/docker.sock");
+        node.start();
+        while (!node.isRunning()) {
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+        api = newKeyValueApi();
+    }
 
-    private KeyValueApi newKeyValueApi() {
+    @AfterClass
+    public static void clean() {
+        System.out.println("Clean stage");
+        api.action(nodeName, NodeAction.DOWN);
+    }
+
+    private static KeyValueApi newKeyValueApi() {
         final String baseUrl = "http://localhost:" + node.getMappedPort(8080);
         return new KeyValueApiHttpClient(baseUrl);
     }
 
     @Test
     public void concurrentPuts() {
-        // TODO simultanious puts for the same key value
+        final String key = "SomeKey";
+        final String value = "SomeValue";
+
+        Thread[] threads = new Thread[100];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> api.put(key, value.getBytes()));
+        }
+
+        for (Thread th : threads) {
+            th.start();
+        }
+
+        for (Thread th : threads) {
+            try {
+                th.join();
+            } catch (InterruptedException e) {
+                // ignore
+            }
+        }
+
+        final String retrievedValue = new String(api.get(key).orElse(Constants.EMPTY_BYTE_ARRAY));
+
+        assertEquals(retrievedValue, value);
     }
 
     @Test
