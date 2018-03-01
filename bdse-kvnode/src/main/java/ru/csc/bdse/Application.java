25f72548f3ffddb4dc6f56e910634e5f99d60722
equivalence1 <equivalence1@gmail.com>
2018-03-01 15:11:14 +0300

    Stop node when exiting Application. Sadly, DBMS container wont be stopped in tests

--- bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/Application.java
@@ -4,16 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.csc.bdse.kv.KeyValueApi;
+import ru.csc.bdse.kv.NodeAction;
 import ru.csc.bdse.kv.db.postgres.PostgresPersistentKeyValueApi;
 import ru.csc.bdse.util.Env;
 
+import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @SpringBootApplication
 public class Application {
 
+    private static KeyValueApi nodeInUse;
+    private static String nodeName;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PreDestroy
+    public static void stop() {
+        System.out.println("stopping node " + nodeName);
+        nodeInUse.action(nodeName, NodeAction.DOWN);
     }
 
     private static String randomNodeName() {
@@ -23,6 +34,8 @@ public class Application {
     @Bean
     KeyValueApi node() {
         String nodeName = Env.get(Env.KVNODE_NAME).orElseGet(Application::randomNodeName);
-        return new PostgresPersistentKeyValueApi(nodeName);
+        Application.nodeName = nodeName;
+        nodeInUse = new PostgresPersistentKeyValueApi(nodeName);
+        return nodeInUse;
     }
 }
